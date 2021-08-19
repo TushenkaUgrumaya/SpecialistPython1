582088e387656103a9a56a07b8354d1c3a03cd24
TushenkaUgrumaya <88528530+TushenkaUgrumaya@users.noreply.github.com>
2021-08-19 20:58:29 +0300

    Update 01_task_dict.py

--- Module3/practice/dict/01_task_dict.py
+++ Module3/practice/dict/01_task_dict.py
@@ -7,4 +7,4 @@
 item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub", "count": "10"}
 dollar_rate = 74.12
 
-# TODO: your code here
+print(float(item['price']) / dollar_rate * int(item['count']))
