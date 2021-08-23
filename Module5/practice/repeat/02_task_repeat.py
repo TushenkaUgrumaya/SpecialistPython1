9b4b44228e3b473f5379913f8a1ee40581d99176
TushenkaUgrumaya <88528530+TushenkaUgrumaya@users.noreply.github.com>
2021-08-23 19:37:51 +0300

    Update 02_task_repeat.py

--- Module5/practice/repeat/02_task_repeat.py
+++ Module5/practice/repeat/02_task_repeat.py
@@ -5,3 +5,17 @@
 
 def palindrome(number):
     pass
+    temp_number = number
+    list_number = []
+    while temp_number != 0:
+        list_number.append(temp_number % 10)
+        temp_number //= 10
+    if list_number[::-1] == list_number:
+        return True
+    return False
+
+print(palindrome(234532))
+print(palindrome(764467))
+print(palindrome(56765))
+# print(palindrome(0023443200))
+print(palindrome(9999999999))
