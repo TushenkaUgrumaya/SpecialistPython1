15a28096f12657210b4c08b9256c0bc5e8d60556
TushenkaUgrumaya <88528530+TushenkaUgrumaya@users.noreply.github.com>
2021-08-25 19:45:01 +0300

    Update 01_task_except.py

--- Module7/practice/exceptions/01_task_except.py
+++ Module7/practice/exceptions/01_task_except.py
@@ -4,3 +4,18 @@
 # На вход программе подается строка формата nxm (x - латинская буква икс).
 # Пример входных данных: 12x6
 # Если данные вводятся в неверном формате, сообщить об этом и запросить ввод заново.
+
+import math
+
+while True:
+    try:
+        size = input('Size =: ')
+        numbers = size.split('x')
+        try:
+            total = math.floor(int(numbers[0])/int(numbers[1]))
+            print(total)
+        except IndexError:
+            print("Еще одно число через х")
+        # print(total)
+    except ValueError:
+        print("Неккоректно ввел")
